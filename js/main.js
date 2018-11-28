7ceef3bac044193cdde2cccca3a35870fc9c5979
Jess Eng <jesseng@Jesss-MacBook-Pro-2.local>
2018-11-28 14:38:48 -0500

    change style

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ function wrangleData(error, top10bechdelTests, top10castGender, top10crewGender,
         "Passes only one test. The film has a few named female characters such as Vanessa, NTW, and Angel Dust, but they never talk with each other.",
         "Passes all three tests, barely. Amanda Waller, Harleen Quinzel, and June Moone have very brief conversations."];
 
-    let top10clips = ["<iframe width=\"480\" height=\"360\" src=\"https://ytcropper.com/embed/At5bfdda67988ed/loop/noautoplay/\" frameborder=\"0\" allowfullscreen></iframe><a href=\"/\" target=\"_blank\">via ytCropper</a>",
+    let top10clips = ["<iframe width=\"480\" height=\"360\" src=\"https://ytcropper.com/embed/At5bfdda67988ed/loop/noautoplay/\" frameborder=\"0\" allowfullscreen></iframe><a href=\"/\" target=\"_blank\"><br>via ytCropper</a>",
     "<iframe width=\"480\" height=\"360\" src=\"https://www.youtube.com/embed/FQSgnwGsnzg\" frameborder=\"0\" allow=\"accelerometer; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
     "<iframe width=\"480\" height=\"360\" src=\"https://ytcropper.com/embed/If5bfdeb8534d84/loop/noautoplay/\" frameborder=\"0\" allowfullscreen></iframe><a href=\"/\" target=\"_blank\">via ytCropper</a>",
     "<iframe width=\"480\" height=\"360\" src=\"https://ytcropper.com/embed/Oq5bfdea0d48fe5/loop/noautoplay/\" frameborder=\"0\" allowfullscreen></iframe><a href=\"/\" target=\"_blank\">via ytCropper</a>",
